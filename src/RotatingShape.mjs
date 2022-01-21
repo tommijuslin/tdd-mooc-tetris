16cdf48260053f028fa2ec071a1fab86e61dd5bb
tommijuslin <tommi.juslin@gmail.com>
2022-01-21 20:29:34 +0200

    Rotating 3x3 shape

--- src/RotatingShape.mjs
+++ src/RotatingShape.mjs
@@ -1,12 +1,40 @@
 export class RotatingShape {
-    shape;
-
     constructor(shape) {
-        this.shape = shape;
+        if (typeof shape == "string") {
+            let rows = shape.replace(/^\s+|\s+$/gm,"").split("\n");
+            this.blocks = [];
+    
+            for (let i = 0; i < rows.length; i++) {
+                this.blocks[i] = rows[i].split('');
+            }
+        } else {
+            this.blocks = shape;
+        }
+    }
+
+    rotateRight() {
+        let rotated = new Array(this.blocks.length);
+
+        for (var i = 0; i < rotated.length; i++) {
+            rotated[i] = new Array(this.blocks.length);
+        }
+
+        for (let row = 0; row < this.blocks.length; row++) {
+            for (let col = 0; col < this.blocks.length; col++) {
+                rotated[col][2 - row] = this.blocks[row][col];
+            }
+        }
+
+        return new RotatingShape(rotated);
     }
 
     toString() {
-        let trimmed = this.shape.replace(/^\s+|\s+$/gm,'') + "\n";
-        return trimmed;
+        let str = "";
+
+        for (let i = 0; i < this.blocks.length; i++) {
+            str += this.blocks[i].join("") + "\n";
+        }
+
+        return str;
     }
 }
